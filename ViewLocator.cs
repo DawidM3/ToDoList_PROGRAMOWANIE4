4cff74b728838f0c520d080384f99551fed2be83
DawidM3 <161303514+DawidM3@users.noreply.github.com>
2024-05-28 18:16:34 +0200

    Add files via upload

--- ViewLocator.cs
+++ ViewLocator.cs
@@ -7,20 +7,25 @@ namespace ToDoList
 {
     public class ViewLocator : IDataTemplate
     {
-        public IControl Build(object data)
+        public Control? Build(object? data)
         {
-            var name = data.GetType().FullName!.Replace("ViewModel", "View");
+            if (data is null)
+                return null;
+
+            var name = data.GetType().FullName!.Replace("ViewModel", "View", StringComparison.Ordinal);
             var type = Type.GetType(name);
 
             if (type != null)
             {
-                return (Control)Activator.CreateInstance(type)!;
+                var control = (Control)Activator.CreateInstance(type)!;
+                control.DataContext = data;
+                return control;
             }
 
             return new TextBlock { Text = "Not Found: " + name };
         }
 
-        public bool Match(object data)
+        public bool Match(object? data)
         {
             return data is ViewModelBase;
         }
